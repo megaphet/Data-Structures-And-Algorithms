4f2d9ac3942f202b69c89418a6e09f0653fd75c2
Japhet <101243202+megaphet@users.noreply.github.com>
2025-09-26 09:52:20 +0200

    Update Airtel queue SIM swap simulation.py
    This simple program uses queue data structure 'airtel_queue' to simulate a queue at Airtel SIM swap service center and prints the customer served second.

--- Exercise3/Queue/Airtel queue SIM swap simulation.py
+++ Exercise3/Queue/Airtel queue SIM swap simulation.py
@@ -1,11 +1,13 @@
+print("\nThis simple program uses queue data structure 'airtel_queue' to simulate a queue at Airtel SIM swap service center and prints the customer served second.\n")
 from collections import deque
 
 # Airtel queue
 airtel_queue = deque(["Eric", "Diane", "Kevin", "Amina", "John", "Teta"])
 
+print(f"People in Airtel queue: {airtel_queue}")
+
 # Serve first two customers
 first_served = airtel_queue.popleft()
 second_served = airtel_queue.popleft()
 
-print(f"1st served: {first_served}")
-print(f"2nd served: {second_served}")
+print(f"2nd served customer is: {second_served}")
